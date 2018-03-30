2d9eb9fe9311c9191b330ec353cbe73914508d75
Matt Farmer <mrfarmer777@gmail.com>
2018-03-30 02:34:40 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,7 +11,7 @@ def position_taken?(board,ind)
 end
 
 def valid_move?(board,ind)
-  if(ind.between?(0,8) && !position_taken(board,ind))
+  if(ind.between?(0,8) && !position_taken?(board,ind))
     true
   else
     false
